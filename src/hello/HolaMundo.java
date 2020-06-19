1e7571d8c45e75060d95b72aef76f06b62225dfd
Andy Liu <Kairos_99@hotmail.com>
2020-06-19 10:24:30 +0200

    Texto D y rama master

--- src/hello/HolaMundo.java
+++ src/hello/HolaMundo.java
@@ -3,15 +3,17 @@ package hello;
 public class HolaMundo {
 	public static void main(String[] args){
 		System.out.println("hellogeuif");
-<<<<<<< HEAD
+
 		System.out.println("Texto B");
 		System.out.println("Texto C");
+		System.out.println("Texto D");
 		
-=======
+
 		System.out.println("Texto F");
 		System.out.println("Texto G");
 		System.out.println("Texto H");
->>>>>>> refs/heads/produccion
+		
+
 	}
 
 }
